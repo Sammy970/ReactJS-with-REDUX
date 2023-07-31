bb8563c415a513e2668689505ba6f19bbb098a20
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-31 09:40:13 +0530

    Attaching Payload

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,6 +9,9 @@ const Counter = () => {
   const incHandler = () => {
     dispatch({ type: "inc" });
   };
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
   const decHandler = () => {
     dispatch({ type: "dec" });
   };
@@ -21,6 +24,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
