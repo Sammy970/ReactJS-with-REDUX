20436ac4e9fb99b838f4eb8667dfcd4deccce3ba
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-31 09:45:50 +0530

    Managing Multiple state data using Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,33 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initalState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initalState, action) => {
   if (action.type === "inc") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "dec") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
